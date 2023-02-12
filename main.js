e1deb9b600981af4ba296d17e288a8fb214675e2
choiinyoung <choi121100@naver.com>
2023-02-12 21:32:47 +0900

    사칙연산

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let num1 = prompt("첫 번째 숫자")
 let num2 = prompt("두 번째 숫자")
 
-let result = num1*num2
+let result1 = num1+num2
+let result2 = num1-num2
+let result3 = num1/num2
+let result4 = num1*num2
 
-alert(result)
-console.log(result)
+alert(`더하기 ${result1}`)
+alert(`빼기 ${result2}`)
+alert(`나누기 ${result3}`)
+alert(`곱하기 ${result4}`)
